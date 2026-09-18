ccb935da6e49f9963d6629fab1af64e28407aff6
Isaac Arias <ariaspicado2007@gmail.com>
2026-09-17 19:20:08 -0600

    agrega horarios.h y horarios.c

--- proyecto-etapa1/Project1/include/constantes.h
+++ proyecto-etapa1/Project1/include/constantes.h
@@ -5,15 +5,19 @@
 
 #define MAX_CODIGO 8
 #define MAX_NOMBRE 150
-#define MAX_REQUISITOS 4
-#define MAX_CORREQUISITOS 4
+#define MAX_REQUISITOS 8
+#define MAX_CORREQUISITOS 8
 #define MAX_CURSOS 100
 #define MAX_LINEA 500
+#define MAX_BLOQUES_HORARIO 300
 
 #define RUTA_PLANM "data/entrada/plan_materiales.txt"
 #define RUTA_PLANC "data/entrada/plan_computadores.txt"
 #define RUTA_HISTORIALM "data/entrada/historialM.txt"
 #define RUTA_HISTORIALC "data/entrada/historialC.txt"
-#define RUTA_SALIDA "data/salida/catalogo.json"
+#define RUTA_HORARIOSM "data/entrada/horariosM.txt"
+#define RUTA_HORARIOSC "data/entrada/horariosC.txt"
+#define RUTA_SALIDA_M "data/salida/catalogo_materiales.json"
+#define RUTA_SALIDA_C "data/salida/catalogo_computadores.json"
 
 #endif
---
+++ proyecto-etapa1/Project1/src/horario.c
@@ -0,0 +1,96 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "horario.h"
+
+#define MAX_CAMPOS 12
+
+static int hora_a_minutos(const char* hhmm) {
+    int h = 0, m = 0;
+    sscanf(hhmm, "%d:%d", &h, &m);
+    return h * 60 + m;
+}
+
+int horarios_chocan(const BloqueHorario* a, const BloqueHorario* b) {
+    if (strcmp(a->dia, b->dia) != 0) return 0;
+    int inicioA = hora_a_minutos(a->hora_inicio);
+    int finA = hora_a_minutos(a->hora_fin);
+    int inicioB = hora_a_minutos(b->hora_inicio);
+    int finB = hora_a_minutos(b->hora_fin);
+    return (inicioA < finB) && (inicioB < finA);
+}
+
+static int es_codigo_curso(const char* texto) {
+    int len = (int)strlen(texto);
+    if (len != 6) return 0;
+    if (!isupper((unsigned char)texto[0]) || !isupper((unsigned char)texto[1])) return 0;
+    for (int i = 2; i < len; i++) {
+        if (!isdigit((unsigned char)texto[i])) return 0;
+    }
+    return 1;
+}
+
+static void agregar_bloques_de_horario(const char* codigo, int grupo, char* horario_texto, BloqueHorario bloques[], int max_bloques, int* num_bloques) {
+    char* token = strtok(horario_texto, " ");
+    while (token != NULL) {
+        char dia[4] = { 0 };
+        char hora_inicio[6] = { 0 };
+        char hora_fin[6] = { 0 };
+        if (sscanf(token, "%3[^[][%5[^-]-%5[^]]]", dia, hora_inicio, hora_fin) == 3) {
+            if (*num_bloques < max_bloques) {
+                strcpy(bloques[*num_bloques].codigo_curso, codigo);
+                bloques[*num_bloques].grupo = grupo;
+                strcpy(bloques[*num_bloques].dia, dia);
+                strcpy(bloques[*num_bloques].hora_inicio, hora_inicio);
+                strcpy(bloques[*num_bloques].hora_fin, hora_fin);
+                (*num_bloques)++;
+            }
+        }
+        token = strtok(NULL, " ");
+    }
+}
+
+int parsear_horarios(const char* ruta, BloqueHorario bloques[], int max_bloques, int* num_bloques) {
+    FILE* archivo = fopen(ruta, "r");
+    if (archivo == NULL) {
+        printf("Error: No se pudo abrir %s\n", ruta);
+        return 0;
+    }
+
+    char linea[MAX_LINEA];
+    *num_bloques = 0;
+    int grupo_actual = 0;
+
+    while (fgets(linea, sizeof(linea), archivo)) {
+        linea[strcspn(linea, "\r\n")] = 0;
+        if (strlen(linea) == 0) continue;
+
+        char* campos[MAX_CAMPOS];
+        int num_campos = 0;
+        char* token = strtok(linea, "\t");
+        while (token != NULL && num_campos < MAX_CAMPOS) {
+            campos[num_campos++] = token;
+            token = strtok(NULL, "\t");
+        }
+        if (num_campos < 2) continue;
+
+        char* codigo = NULL;
+        for (int i = 0; i < num_campos; i++) {
+            if (es_codigo_curso(campos[i])) {
+                codigo = campos[i];
+                break;
+            }
+        }
+        if (codigo == NULL) continue;
+
+        char* horario = campos[num_campos - 1];
+
+        grupo_actual++;
+        agregar_bloques_de_horario(codigo, grupo_actual, horario, bloques, max_bloques, num_bloques);
+    }
+
+    fclose(archivo);
+    return 1;
+}
