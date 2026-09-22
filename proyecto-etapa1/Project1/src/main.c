12078533161a5cddd99a2f6e703dcf2603018fbd
Jose Andrés Chavarría <joseandreschava09@gmail.com>
2026-09-22 03:23:34 -0600

    Agrega marcar_aprobados y procesar_carrera; main llama al pipeline completo por carrera

--- proyecto-etapa1/Project1/src/main.c
+++ proyecto-etapa1/Project1/src/main.c
@@ -4,10 +4,43 @@
 #include <string.h>
 #include "curso.h"
 #include "parser.h"
+#include "horario.h"
+#include "catalogo.h"
+#include "choques.h"
 #include "../include/constantes.h"
 #include <windows.h>
 
 #define MAX_HISTORIAL 100
+
+static int procesar_carrera(const char* ruta_plan, const char* ruta_historial, const char* ruta_horarios,
+    Curso cursos[], int* num_cursos) {
+    memset(cursos, 0, sizeof(Curso) * MAX_CURSOS);
+
+    if (!parsear_plan_estudios(ruta_plan, cursos, num_cursos)) {
+        printf("Error al cargar el plan de estudios\n");
+        return 0;
+    }
+
+    char historial[MAX_HISTORIAL][MAX_CODIGO];
+    int num_historial = 0;
+    if (!cargar_historial(ruta_historial, historial, &num_historial)) {
+        printf("Error al cargar el historial\n");
+        return 0;
+    }
+
+    BloqueHorario bloques[MAX_BLOQUES_HORARIO];
+    int num_bloques = 0;
+    if (!parsear_horarios(ruta_horarios, bloques, MAX_BLOQUES_HORARIO, &num_bloques)) {
+        printf("Error al cargar los horarios\n");
+        return 0;
+    }
+
+    asociar_horarios(cursos, *num_cursos, bloques, num_bloques);
+    marcar_aprobados(cursos, *num_cursos, historial, num_historial);
+    detectar_choques(cursos, *num_cursos);
+
+    return 1;
+}
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
@@ -57,8 +90,8 @@ int main() {
         printf("\n");
     }
 
+
     // TODO: Validar requisitos
-    // TODO: Detectar choques (necesita horarios)
     // TODO: Exportar JSON
 
     return 0;
